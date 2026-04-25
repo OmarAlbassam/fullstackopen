c86538fdba3962191f20c42ad7be09ab093da498
OmarAlbassam <omar59056@gmail.com>
2026-04-25 13:59:53 +0300

    1.3

---
+++ test.js
@@ -0,0 +1,19 @@
+// for(var i = 0; i<3; i++) {
+//   console.log(i)
+// }
+// console.log(i)
+
+
+// for(let j = 0; j<3; j++) {
+//   console.log(j)
+// }
+// console.log(j)
+
+
+
+
+const arr = [1,2,3]
+console.log(arr)
+arr.push(4)
+
+console.log(arr)

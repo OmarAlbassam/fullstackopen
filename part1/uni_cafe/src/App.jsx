a07ea2aefee9c2b0b08faf09db097b4e33d0afae
OmarAlbassam <omar59056@gmail.com>
2026-04-29 20:48:09 +0300

    1.7

--- part1/uni_cafe/src/App.jsx
+++ part1/uni_cafe/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
 
 
 
 const App = () => {
+
+
+
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad;
+  const average = ((good - bad)/total)
+  const sentimentAverage = (good/total) *100
   return (
     <div>
       <h1>give feedback</h1>
@@ -19,7 +26,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
-
+      <p>all: {total}</p>
+      <p>average: {average}</p>
+      <p>positive: {sentimentAverage}%</p>
     </div>
   )
 }

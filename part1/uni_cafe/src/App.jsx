b42b795bd263611f3f79464f5184b5a671b5729a
OmarAlbassam <omar59056@gmail.com>
2026-04-30 08:59:37 +0300

    1.8

--- part1/uni_cafe/src/App.jsx
+++ part1/uni_cafe/src/App.jsx
@@ -1,7 +1,24 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + bad + neutral
 
+  const average = ((good - bad)/total)
+  const positive = (good/total) *100
 
+  return (
+    <div>
+      <h1>stats</h1>
+      <p>good: {good}</p>
+      <p>neutral: {neutral}</p>
+      <p>bad: {bad}</p>
+      <p>all: {total}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}%</p>
+
+    </div>
+  )
+}
 
 const App = () => {
 
@@ -11,10 +28,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const total = good + neutral + bad;
-  const average = ((good - bad)/total)
-  const sentimentAverage = (good/total) *100
+  
   return (
     <div>
       <h1>give feedback</h1>
@@ -22,13 +36,8 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      <h1>stats</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>average: {average}</p>
-      <p>positive: {sentimentAverage}%</p>
+      
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }

6e8fb4bb1b2fa066fc7288d934bea81f7d59667f
OmarAlbassam <omar59056@gmail.com>
2026-04-30 09:06:50 +0300

    1.9

--- part1/uni_cafe/src/App.jsx
+++ part1/uni_cafe/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-const Statistics = ({good, neutral, bad}) => {
-  const total = good + bad + neutral
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + bad + neutral;
 
-  const average = ((good - bad)/total)
-  const positive = (good/total) *100
-
-  return (
-    <div>
-      <h1>stats</h1>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>average: {average}</p>
-      <p>positive: {positive}%</p>
-
-    </div>
-  )
-}
+  const average = (good - bad) / total;
+  const positive = (good / total) * 100;
+  if (total !== 0) {
+    return (
+      <div>
+        <h1>stats</h1>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {total}</p>
+        <p>average: {average}</p>
+        <p>positive: {positive}%</p>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>stats</h1>
+        <p>no feedback given</p>
+      </div>
+    );
+  }
+};
 
 const App = () => {
-
-
-
   // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -36,10 +40,9 @@ const App = () => {
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
 
-      
       <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;

e8145feff486f30ab0f5bec81948c1848f09e986
OmarAlbassam <omar59056@gmail.com>
2026-06-07 15:47:17 +0300

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,17 +6,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "040-1234567" }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [search, setSearch] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
   if (!persons.some(person => person.name === newName) && !persons.some(person => person.number === newNumber)) {
-    console.log(newName)  
-    console.log(newNumber)
     const newPersons = persons.concat({ name: newName, number: newNumber })
       setPersons(newPersons)
       setNewName('')
@@ -34,9 +36,20 @@ const App = () => {
 const handleNumberChange = (event) => {
   setNewNumber(event.target.value)
  }
+
+ const handleSearchChange = (event) => {
+  setSearch(event.target.value)
+ }
+ 
+   const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(search.toLowerCase())
+  )
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={search} onChange={handleSearchChange}/></div>
+
+      <h2>add new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={handleNameChange}/></div>
         <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
@@ -44,7 +57,7 @@ const handleNumberChange = (event) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, i) =>
+        {filteredPersons.map((person, i) =>
           <li key={i}>{person.name} {person.number}</li>
         )}
       </ul>

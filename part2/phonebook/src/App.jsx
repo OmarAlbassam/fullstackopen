8dff58ddbe1a8658c0905bc635de03efc49ab6c8
OmarAlbassam <omar59056@gmail.com>
2026-06-07 14:18:29 +0300

    2.5

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,27 +1,20 @@
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+import { useState } from 'react'
+import Note from './components/Note'
 
-  return <Course course={course} />
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
 }
 
-export default App
+export default App 

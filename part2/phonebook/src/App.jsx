920ab6083fdc9c64654d710b64b04be914a39944
OmarAlbassam <omar59056@gmail.com>
2026-06-10 14:57:36 +0300

    2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect } from 'react'
+import axios, { Axios } from 'axios'
 const Filter = ({ search, handleSearchChange }) => (
   <div>filter shown with <input value={search} onChange={handleSearchChange}/></div>
 )
@@ -35,16 +35,18 @@ const Numbers = ({filteredPersons}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
+
+  useEffect((() => {
+    axios.get('http://localhost:3001/persons ').then(response => {
+      setPersons(response.data)
+    })
+  }),
+  [])
+
 
 const addPerson = (event) => {
   event.preventDefault()
